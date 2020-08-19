18dfe4ce860d13981a4ac286bde0242ebc7fef20
Thomas Augustsson <thomas@understandit.se>
2020-08-19 22:17:43 +0200

    Refactored to only change classes on visible elements to improve performance

--- width-modifier.js
+++ width-modifier.js
@@ -6,17 +6,39 @@
  * See Philip Walton's Responsive Components: a Solution to the Container Queries Problem
  * https://philipwalton.com/articles/responsive-components-a-solution-to-the-container-queries-problem/
  *
- * Implementation basd on https://github.com/pinkhominid/wc-responsive-container
+ * Implementation based on https://github.com/pinkhominid/wc-responsive-container
+ *
+ * How to use
+ * Any elements with the class width-modifier will be evaluated
+ * Add the custom property --width-modifier on the format
+ * --width-modifier: class1 0 200, class2 200 500, class3 500;
+ * To have width-modifer set class1 when the element is >= 0 and < 200,
+ * class2 when >= 200 and < 500 and class3 when >= 500
+ * It is also possible to use custom properties as values like
+ * --width-modifier: class 0 var(--max);
  */
 
 (function(document) {
 
-const CLASS = 'width-modifier';
+/*
+ * Keeps track of references to vidth-modifier nodes style object to minimize calls to getComputedStyle
+ */
+const nodesStyle = new WeakMap();
+
+/**
+ * Keep a list of width-modifier nodes with resize changes since last updateClasses
+ */
+const nodesWithResize = new WeakMap();
 
 /*
- * Keeps track of references to nodes style object to minimize calls to getComputedStyle
+ * Keep trach of all width-modifier nodes in the viewport at any given time
  */
-const ns = new WeakMap();
+const nodesInViewPort = new WeakMap();
+
+/*
+ * Keeps track of recently changed nodes to prevent loops
+ */
+const recentChanges = new WeakMap();
 
 /*
  *
@@ -27,53 +49,90 @@ const mo = new MutationObserver(changes => {
       if(node.nodeType != 1) {
         return;
       }
-      
-      if(!node.classList.contains(CLASS)) {
+      if(!node.classList.contains('width-modifier')) {
         return;
       }
-      ns.set(node, getComputedStyle(node));
-      ro.observe(node);
+      trackNode(node);
     });
     change.removedNodes.forEach(node => {
       if(node.nodeType != 1) {
         return;
       }
-      if(!node.classList.contains(CLASS)) {
+      if(!node.classList.contains('width-modifier')) {
         return;
       }
-      ro.unobserve(node);
-      ns.delete(node); 
+      untrackNode(node);
     });
   });
 }).observe(document, {childList: true, subtree: true});
 
 // Create a single observer for all responsive elements
+const io = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      nodesInViewPort.set(entry.target, true);
+      if(nodesWithResize.has(entry.target)) {
+        updateClasses(entry.target, nodesWithResize.get(entry.target));
+        nodesWithResize.delete(entry.target);
+      }
+    } else {
+      nodesInViewPort.delete(entry.target);
+    }
+  });
+}, { rootMargin: '400px' });
+
+// Create a single observer for all responsive elements
 const ro = new ResizeObserver(entries => {
   entries.forEach(entry => {
-    updateClasses(entry.target);
+    if(nodesInViewPort.has(entry.target)) {
+      updateClasses(entry.target, entry.contentRect);
+    } else {
+      // Added node to be processed when in viewport
+      nodesWithResize.set(entry.target, entry.contentRect);
+    }
   });
 });
 
-function updateClasses(node) {
-  contentRect = node.getBoundingClientRect();
+function trackNode(node) {
+  nodesStyle.set(node, getComputedStyle(node));
+  io.observe(node);
+  ro.observe(node);
+}
 
-  breakpoints = ns.get(node).getPropertyValue('--' + CLASS);
+function untrackNode(node) {
+  ro.unobserve(node);
+  io.unobserve(node);
+  nodesStyle.delete(node); 
+}
+
+function updateClasses(node, contentRect) {
+  if(recentChanges.has(node)) {
+    return;
+  }
+
+  breakpoints = nodesStyle.get(node).getPropertyValue('--width-modifier');
 
   if(!breakpoints) {
     return;
   }
 
   // Update the matching breakpoints on the observed element.
-  breakpoints.trim().split(' ').map(breakpoint => {
-    return breakpoint.split('|');
+  breakpoints.trim().split(',').map(breakpoint => {
+    return breakpoint.replace(/  */g, ' ').trim().split(' ');
   }).forEach(breakpoint => {
     const className = breakpoint[0];
     const minWidth = breakpoint.length > 1 ? parseFloat(breakpoint[1]) : 0;
     const maxWidth = breakpoint.length > 2 ? parseFloat(breakpoint[2]) : 99999;
     if (contentRect.width >= minWidth && contentRect.width < maxWidth) {
-      node.classList.add(className);
+      if(!node.classList.contains(className)) {
+        recentChanges.set(node, setTimeout(() => {recentChanges.delete(node);}, 50));
+        node.classList.add(className);
+      }
     } else {
-      node.classList.remove(className);
+      if(node.classList.contains(className)) {
+        recentChanges.set(node, setTimeout(() => {recentChanges.delete(node);}, 50));
+        node.classList.remove(className);
+      }
     }
   });
 }
@@ -81,10 +140,9 @@ function updateClasses(node) {
 /*
  * Parse over the current element tree to handle any pre-existing matching nodes
  */
-const elements = document.getElementsByClassName(CLASS);
+const elements = document.getElementsByClassName('width-modifier');
 for (var i = 0; i < elements.length; i++) {
-  ns.set(elements[i], getComputedStyle(elements[i]));
-  ro.observe(elements[i]);
+  trackNode(elements[i]);
 }
 
 })(document);
